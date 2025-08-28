c25a4ea31318043d8f77f1893ee895762e7e12cf
Jo Greenlee <sweettroubles29@gmail.com>
2025-08-28 13:05:05 -0500

    setProperty and onEvents Added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 setProperty("WaterAppHeader", "color", "#38618C");
+setProperty("ParaText", "color", "#38618C");
+setProperty("WaterAppHeader", "font-size", "36px");
+setProperty("ParaText", "font-size", "20px");
+setProperty("SpanishButton", "background-color", "#38618C");
+setProperty("EnglishButton", "background-color", "#38618C");
+
+onEvent ("SpanishButton", "click", function() {
+  setText("WaterAppHeader", "Proyecto de Conservación de Agua");
+  setText("ParaText", "Es importante que todos hagamos nuestra parte para usar menos agua. Haga clic en esta aplicación para obtener consejos sobre ideas de conservación.");
+  setText("SpanishButton", "Español");
+  setText("EnglishButton", "Inglés");
+});
+onEvent ("EnglishButton", "click", function() {
+  setText("WaterAppHeader", "Water Conservation Project");
+  setText("ParaText", "It's important that we all do our part to use less water. Click through this app for tips on conservation ideas.");
+  setText("SpanishButton", "Spanish");
+  setText("EnglishButton", "English");
+});
