9b0d039cac9d629c73d64f716ef787300cf2055e
Jo Greenlee <sweettroubles29@gmail.com>
2025-09-03 10:48:50 -0500

    added js

--- script.js
+++ script.js
@@ -5,15 +5,38 @@ setProperty("ParaText", "font-size", "30px");
 setProperty("SpanishButton", "background-color", "#38618C");
 setProperty("EnglishButton", "background-color", "#38618C");
 
-onEvent ("SpanishButton", "click", function() {
+onEvent("SpanishButton", "click", function () {
   setText("WaterAppHeader", "Proyecto de Conservación de Agua");
   setText("ParaText", "Es importante que todos hagamos nuestra parte para usar menos agua. Haga clic en esta aplicación para obtener consejos sobre ideas de conservación.");
   setText("SpanishButton", "Español");
   setText("EnglishButton", "Inglés");
 });
-onEvent ("EnglishButton", "click", function() {
+onEvent("EnglishButton", "click", function () {
   setText("WaterAppHeader", "Water Conservation Project");
   setText("ParaText", "It's important that we all do our part to use less water. Click through this app for tips on conservation ideas.");
   setText("SpanishButton", "Spanish");
   setText("EnglishButton", "English");
+});
+
+var correctBottle = false;
+
+hideElement("plasticNo");
+hideElement("drip");
+
+onEvent("glassBottle", "click", function () {
+  hideElement("plasticNo");
+  correctBottle = true;
+});
+
+onEvent("plasticBottle", "click", function () {
+  showElement("plasticNo");
+  correctBottle = false;
+});
+
+onEvent("faucet", "click", function () {
+  if (correctBottle) {
+    showElement("drip");
+  } else {
+    hideElement("drip");
+  }
 });
